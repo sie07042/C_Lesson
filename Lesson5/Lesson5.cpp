5dd1d1918782c2aeb8cb234f30ac74fc92daeada
sie07042 <sie07042@naver.com>
2025-05-20 21:49:02 +0900

    비트연산자까지 완료

---
+++ Lesson5/Lesson5.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <stdio.h>
+
+int main()
+{
+    //비트 연산자 : <<, >>, &, |, ~, ^(eXclusive OR)
+    short n3 = 0;
+    short n1 = 3; //0011
+    short n2 = 5; //0101
+    //n3 = n1 & n2; //0001
+    n3 = n1 | n2; //0111
+    printf("n3 = %d\n", n3);
+
+    n3 = ~n1;
+    printf("n3 = %d\n", n3);
+
+    n3 = n1^n2;
+    printf("n3 = %d\n", n3);
+
+    // 0011 : 1100
+    n3 = n1 << 2; // n1을 왼쪽으로 2비트 이동시킴
+    printf("n3 = %d\n", n3);
+
+    // 0101 : 0001
+    n3 = n2 >> 2; 
+    printf("n3 = %d\n", n3);
+
+    return 0;
+}
+
