247cdbd9c804840402d81e7babda3267cd74b132
sie07042 <sie07042@naver.com>
2025-05-22 21:39:08 +0900

    스위치 케이스문

--- Lesson7/Lesson7.cpp
+++ Lesson7/Lesson7.cpp
@@ -27,38 +27,67 @@ int main()
 	*/
 
 	// 사용자로부터 메뉴를 선택받아서 해당 메뉴를 출력하는 코드를 작성
-	printf("1. 새파일\n");
-	printf("2. 파일열기\n");
-	printf("3. 파일저장\n");
-	printf("4. 인쇄\n");
-	printf("=================\n");
-	printf("0. 프로그램 종료\n");
-	printf("=================\n");
+	//printf("1. 새파일\n");
+	//printf("2. 파일열기\n");
+	//printf("3. 파일저장\n");
+	//printf("4. 인쇄\n");
+	//printf("=================\n");
+	//printf("0. 프로그램 종료\n");
+	//printf("=================\n");
 
-	printf("메뉴 번호를 선택하여 입력하세요 >>>");
-	int menu;
-	scanf("%d", &menu);
-	printf("선택된 메뉴는 %d\n", menu);
+	//printf("메뉴 번호를 선택하여 입력하세요 >>>");
+	//int menu;
+	//scanf("%d", &menu);
+	//printf("선택된 메뉴는 %d\n", menu);
 
-	switch (menu)
+	//switch (menu)
+	//{
+	//case 1:
+	//	printf("새파일이 선택되었습니다.\n");
+	//	break;
+	//case 2:
+	//	printf("파일열기가 선택되었습니다.\n");
+	//	break;
+	//case 3:
+	//	printf("파일저장이 선택되었습니다.\n");
+	//	break;
+	//case 4:
+	//	printf("인쇄가 선택되었습니다.\n");
+	//	break;
+	//case 0:
+	//	printf("프로그램을 종료합니다.\n");
+	//	break;
+	//default:
+	//	printf("알 수 없는 값이 입력되었습니다.\n");
+	//}
+
+	// 문제)
+	// switch cass문을 사용하여 코드를 작성하세요.
+	// 출력형식)
+	// 점수는 90점이고, A학점입니다.
+	int score;
+	int grade;
+	printf("점수를 입력하세요(0~100점까지) >>>");
+	scanf("%d", &score);
+	switch (score /10)
 	{
-	case 1:
-		printf("새파일이 선택되었습니다.\n");
+	case 10:
+	case 9:
+		grade = 'A';
 		break;
-	case 2:
-		printf("파일열기가 선택되었습니다.\n");
+	case 8:
+		grade = 'B';
 		break;
-	case 3:
-		printf("파일저장이 선택되었습니다.\n");
+	case 7:
+		grade = 'C';
 		break;
-	case 4:
-		printf("인쇄가 선택되었습니다.\n");
-		break;
-	case 0:
-		printf("프로그램을 종료합니다.\n");
+	case 6:
+		grade = 'D';
 		break;
 	default:
-		printf("알 수 없는 값이 입력되었습니다.\n");
+		grade = 'F';
 	}
+
+	printf("점수는 %d점이고, %c학점입니다.\n", score, grade);
 	return 0;
 }
