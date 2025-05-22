e13ebe9dc5aaaeabe3d253e35bc6a116a9c657e6
sie07042 <sie07042@naver.com>
2025-05-22 21:16:21 +0900

    스위치 케이스문

--- Lesson6/Lesson6.cpp
+++ Lesson6/Lesson6.cpp
@@ -1,4 +1,5 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
 int main()
 {
@@ -46,9 +47,10 @@ int main()
         위에 모든 조건식들이 거짓일대 실행되는 명령문들
      }
     */
-
+    
+    // n = 5;
     // if문의 첫번째 형식
-    if (n % 2 == 0)
+    if (!(n % 2))
         printf("%d는 짝수입니다.\n", n);
 
     if (n % 2 == 0) 
@@ -56,7 +58,39 @@ int main()
     else
         printf("%d는 홀수입니다.\n", n);
 
-    
+    // 나이에 따른 버스요금 결정하기
+    int age = 15;
+    int payment;
+
+    if (age >= 65)
+        payment = 0;
+    else if (age < 65 && age >18) // 18<age<65
+        payment = 1500;
+    else if (age <= 18 && age >= 8)
+        payment = 500;
+
+    // 문제)
+    // 출력형식)
+    // 점수는 90점이고, A학점입니다.
+    int score;
+    int grade;
+    printf("점수를 입력하세요(0~100점까지) >>>");
+    scanf("%d", &score);
+
+    if (score >= 90)
+        grade = 'A';
+    else if (score >= 80)
+        grade = 'B';
+    else if (score >= 70)
+        grade = 'C';
+    else if (score >= 60)
+        grade = 'D';
+    else
+        grade = 'F';
+
+    printf("점수는 %d점이고, %c학점입니다.\n", score,grade);
+
+
     return 0;
 }
 
---
+++ Lesson7/Lesson7.cpp
@@ -0,0 +1,64 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{	
+	int n = 1 + 5;
+
+	/*
+	switch (변수(또는 함수, 수식))
+	{
+	case n1:
+		위 변수가 n1과 같을때 실행되는 명령문들...;
+		break;
+	case n2:
+		위 변수가 n2과 같을때 실행되는 명령문들...;
+		break;
+	case n3:
+		위 변수가 n3과 같을때 실행되는 명령문들...;
+		break;
+	case n4:
+		위 변수가 n4과 같을때 실행되는 명령문들...;
+		break;
+	...
+	default:
+		위 변수가 모든 case에 해당되지 않을때 실행되는 명령문들...;
+	}
+	*/
+
+	// 사용자로부터 메뉴를 선택받아서 해당 메뉴를 출력하는 코드를 작성
+	printf("1. 새파일\n");
+	printf("2. 파일열기\n");
+	printf("3. 파일저장\n");
+	printf("4. 인쇄\n");
+	printf("=================\n");
+	printf("0. 프로그램 종료\n");
+	printf("=================\n");
+
+	printf("메뉴 번호를 선택하여 입력하세요 >>>");
+	int menu;
+	scanf("%d", &menu);
+	printf("선택된 메뉴는 %d\n", menu);
+
+	switch (menu)
+	{
+	case 1:
+		printf("새파일이 선택되었습니다.\n");
+		break;
+	case 2:
+		printf("파일열기가 선택되었습니다.\n");
+		break;
+	case 3:
+		printf("파일저장이 선택되었습니다.\n");
+		break;
+	case 4:
+		printf("인쇄가 선택되었습니다.\n");
+		break;
+	case 0:
+		printf("프로그램을 종료합니다.\n");
+		break;
+	default:
+		printf("알 수 없는 값이 입력되었습니다.\n");
+	}
+	return 0;
+}
