5f5434a10f44ae2bf115ee7d22a33e7f0ff25b1d
sie07042 <sie07042@naver.com>
2025-05-29 21:53:01 +0900

    함수

--- Lesson10/Lesson10.cpp
+++ Lesson10/Lesson10.cpp
@@ -1,5 +1,6 @@
 ﻿#include <stdio.h>
 
+
 int main()
 {   
     // for(초기식; 조건식; 증감식)
@@ -32,6 +33,93 @@ int main()
 
         printf("\n");
     }
+
+
+    // 1 2 3 4 5 6 7 8 9 10
+    // 11 12 13 14 15 16 17 18 19 20
+
+    for (int i = 1; i <= 100; i++)
+    {
+        printf("%d\t", i);
+        if (i % 10 == 0)
+            printf("\n");
+    }
+
+    // 1 2 3 4 5 6 7 8 9 10
+    // 11 12 13 14 15 16 17 18 19 20
+
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0)
+            printf("c\t");
+        else {
+            printf("% d\t", i);
+
+        }
+
+        if (i % 10 == 0)
+            printf("\n");
+    }
+
+    printf("\n");
+
+    for (int i = 1; i <= 100; i++)
+    {   
+        int ten = i / 10; //i에서 10의 자리를 분리
+        int il = i % 10; //i에서 1의 자리를 분리
+        char ch = 'a';
+
+        //10
+        if (ten % 3 == 0 && ten !=0) //10의 자리수가 3의 배수이고 0이 아니라면
+            ch = 'c'; // ch의 값을 'c'로 변경
+        
+        if (il % 3 == 0 && ch == 'c' && il != 0) // 1의 자리수가 3의 배수이고 10의 자리도 3의 배수
+            ch = 'C'; //ch에 'C'로 변경 
+        else if (il % 3 == 0 && il != 0) //그렇지않고 1의 자리수만 3의 배수라면 
+            ch = 'c'; // ch에 'c'를 대입
+
+        if (ch == 'c' || ch == 'C')//ch의값이 'a'가 아니고, 'c'나 'C'라면 숫자대신 ch를 출력
+            printf("%c\t", ch);
+        else 
+            printf("% d\t", i); //위의 경우에 해당하지 않는다면 i를  그대로 출력
+
+        if (i % 10 == 0)
+            printf("\n");
+    }
+
+    printf("\n");
+    
+    for (int i = 1; i <= 100; i++) {
+        int ten = i / 10;  
+        int il = i % 10;   
+
+        if (ten % 3 == 0 && ten != 0) {
+            if (il % 3 == 0 && il != 0)
+                printf("C");
+            else
+                printf("c");
+        }
+        else {
+            printf("%d", ten);  
+        }
+
+        if (il % 3 == 0 && il != 0) {
+            if (ten % 3 == 0 && ten != 0)
+                printf("C");
+            else
+                printf("c"); 
+        }
+        else {
+            printf("%d", il);  
+        }
+
+        printf("\t");
+
+        if (i % 10 == 0) {
+            printf("\n");
+        }
+    }
+   
 
 
     return 0;
---
+++ Lesson11/Lesson11.cpp
@@ -0,0 +1,96 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+// 함수 사용 규칙
+// 1. 호출하기전에 반드시 함수가 정의되어 있어야 한다.
+// 2. 함수를 호출할때는 정의된 형식과 완전하게 동일하게 호출해야한다.
+// 3. 그 외 변수명 작성 규칙과 동일하게 정의한다.
+// 4. 함수는 다른 함수 내부에 포함될수 없다. 반드시 독립적으로 존재해야 한다.
+
+// 함수를 사용하는 이유는 : 코드의 재사용(생산성 높인다, productivity)
+
+// 함수를 정의하는 방법
+// 함수형(자료형) 함수명( [매개변수1, 매개변수2, ...]) {}
+//
+// 매개변수 : Parameters, Argments, 인수, 인자 (호출부로부터 초기화되는 지역변수)
+//
+// 함수의 정의
+int add_number(int n1, int n2) {
+    
+    return n1 + n2; //retun 함수를 처리하고 결과를 호출부로 반환하는 명령
+}
+
+int add_number(int n1, int n2, int n3) {
+
+    return n1 + n2 + n3; //retun 함수를 처리하고 결과를 호출부로 반환하는 명령
+}
+
+// 보이드 : 반환값이 없음, 호출한쪽에 결과를 반환하지 않는다.
+void display_number(int n)
+{
+    printf("매개변수 : %d\n", n);
+}
+
+float get_pi() {
+    return 3.14F;
+}
+
+int mutiply(int n1, int n2) {
+    // retun n1 * n2;
+    int result = 0;
+    for (int i = 0; i < n2; i++) {
+        result += add_number(result, n1);
+    }
+    return result;
+}
+
+// 1부터 n까지의 합을 구하여 반환하는 함수를 정의하고,
+// main()함수에서 100을 입력하여 호출한 뒤 그 결과를 출력하세요.
+int sum(int n1) {
+    int result = 0; // 누적변수
+    for (int i = 1; i <= n1; i++) {
+        result += i; 
+    }
+    return result; 
+}
+
+// 반지름을 입력하면 원의 넓이 값을 구하는 함수를 정의하세요
+// 매개변수로 반지름을 입력받도록 함수를 설계하세요.
+
+float circle(float n1) {
+    return get_pi()* n1 * n1;
+}
+
+
+
+int main()
+{
+    // 변수형 변수명 [= 초기화할 값];
+
+    // Call a function (함수의 호출)
+    int n = add_number(10, 20); // add_number() 함수를 호출하고 그 결과를 n에 저장한다.
+    printf("n = %d\n",n);
+
+    n = add_number(30, 5);
+    printf("n = %d\n", n);
+
+    /*int n1;
+    int n2;
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+    n = add_number(n1, n2);*/
+
+    display_number(50);
+
+    printf("파이 : %.4f\n", get_pi());
+
+    printf("%d\n", mutiply(4,6));
+
+    printf("1부터 100까지의 합 : %d\n", sum(100));
+
+    printf("원의 넓이 : %f\n", circle(3.5));
+
+    return 0;
+}
+
+
