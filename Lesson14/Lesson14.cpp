3e5e18ca7d350a07d5e13b22117fb74380c7427f
sie07042 <sie07042@naver.com>
2025-06-05 21:43:40 +0900

    포인터변수

---
+++ Lesson14/Lesson14.cpp
@@ -0,0 +1,105 @@
+﻿#include <stdio.h>
+
+// 1111 0000
+// F    F
+
+int add(int a, int b) {
+
+    a = 100;
+    b = 200;
+
+    return a + b;
+}
+
+// 함수의 오버로딩(Over Loading)
+int add(int *p1, int *p2) {
+
+    printf("%d\n", *p1);
+    printf("%d\n", *p2);
+    *p1 = 100;
+    *p2 = 200;;
+
+    return *p1 + *p2;
+}
+
+void add(int a, int b, int* ret) {
+
+    *ret = a + b;
+}
+
+void exchange(int *a, int *b) {
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+
+int main()
+{   // Local(지역)
+    //
+    short num; //메모리 2 바이트 확보(정수형으로, 시스템이 알아서 잡으세요)
+    num = 5; // 확보된 메모리 num공간에 정수 5를 대입하라
+
+    // &의 사용처
+    // 논리 연산자 : 1 && 0
+    // 비트 연산자 : 1 & 0
+    // 주소 연산자 : &변수, 변수의 주소
+
+    printf("num = %d\n", num);
+    printf("num의 크기 %ubytes\n", sizeof(num));
+    printf("num의 주소 : %p\n", &num);
+
+    // Pointer(포인터) 변수 : 무조건 주소만 저장할 수 있다.
+    // 자료형* 변수명;
+
+    short* addr = &num; // short* 형을 갖는 변수 addr을 선언하고, num의 주소를 저장한다.
+    printf("addr의 크기 : %ubytes\n", sizeof(addr));
+    printf("addr의 값 : %p\n", addr);
+
+    //addr = 100; 이런 코드는 안됨
+    printf("addr이 참조하는 곳의 값 : %d\n", *addr);
+    *addr = 20;
+    printf("num = %d\n", num);
+
+
+    long long num2 = 200000L;
+    printf("num2 = %lld\n", num2);
+    printf("num2의 크기 %ubytes\n", sizeof(num2));
+    printf("num2의 주소 : %p\n", &num2);
+    long long* addr2 = &num2;
+    printf("addr2의 크기 : %ubytes\n", sizeof(addr2));
+    printf("addr2의 값 : %p\n", addr2);
+    printf("addr2이 참조하는 곳의 값 : %d\n", *addr2);
+
+    int n1 = 10;
+    int n2 = 20;
+    int n3 = add(n1, n2); // 값에 의한 호출(Call by Value)
+
+    printf("n1 = %d\n", n1);
+    printf("n2 = %d\n", n2);
+    printf("n3 = %d\n", n3);
+
+    n3 = add(&n1, &n2); // 참조에 의한 호출(Call by Reference)
+    printf("n1 = %d\n", n1);
+    printf("n2 = %d\n", n2);
+    printf("n3 = %d\n", n3);
+
+    printf("=================\n");
+    n1 = 5;
+    n2 = 8;
+    add(n1, n2, &n3);
+    printf("n1 = %d\n", n1);
+    printf("n2 = %d\n", n2);
+    printf("n3 = %d\n", n3);
+
+    int tmp = n1;
+    /*n1 = n2;
+    n2 = tmp;*/
+    printf("=================\n");
+    exchange(&n1, &n2);
+    printf("n1 = %d\n", n1);
+    printf("n2 = %d\n", n2);
+
+    return 0;
+}
+
---
+++ Lesson15/Lesson15.cpp
@@ -0,0 +1,74 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// 배열을 전달받아서 배열의 모든 요소들의 내용을 출력하기
+// 함수로 배열을 전달할때는 반드시 배열의 시작 주소와 배열의 길이를 같이 전달해야한다.
+void display_array(int p[], int count) {
+
+    printf("[display_array] p의 크기 : %u\n", sizeof(p));
+    printf("p[0] = %d\n", p[0]);
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d\t", p[i]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    /*int num;
+    printf("정수를 입력하세요 >>>");
+    scanf("%d", &num);
+    printf("입력된 정수 : %d\n", num);*/
+
+    char sz[] = "abcde";
+    // 배열명은 주소(상수)이다.
+    /*char sz[30] = { 0 };*/
+    //sz[0] = 'A'; // "A" == sz
+    /*printf("문자열을 입력하세요 >>>");
+    scanf("%s", sz);*/
+    printf("입력된 문자열 : %s\n", sz);
+
+    int num = 5;
+    printf("num의 주소 : %p\n", &num);
+    printf("sz의 주소 : %p\n", sz);
+
+    char* ptr = sz; // 가능
+    //sz = ptr; //불가능
+    printf("%c\n", sz[0]);
+    printf("%c\n", ptr[0]);
+
+    int arr[] = { 10,20,30,40,50 };
+    int* ptr2 = arr;
+    printf("%d\n", arr[2]);
+    printf("%d\n", ptr2[2]);
+    printf("arr의 크기 : %u\n", sizeof(arr));
+    printf("arr의 크기 : %u\n", sizeof(ptr2));
+
+    int count = sizeof(arr) / sizeof(int); //갯수를 구함
+    display_array(arr,count);
+
+    return 0;
+}
+
+// 소문자를 대문자로 출력하는 함수를 작성하세요
+// char sz[] = abcde";
+/*
+A
+B
+C
+D
+E
+*/
+
+// 다음과 같은 형식으로 출력되는 함수를 작성하세요
+// char sz[] = abcde";
+/*
+abcde
+bcbe
+cde
+de
+e
+*/
+
+
