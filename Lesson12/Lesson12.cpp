ee50235a74bc4d640a857faa9e38856c07e6fb35
sie07042 <sie07042@naver.com>
2025-06-05 21:43:40 +0900

    배열

---
+++ Lesson12/Lesson12.cpp
@@ -0,0 +1,67 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+	char ch =1;
+	short sh = 2;
+	int num = 3;
+
+	float r1 = 1.0;
+	double r2;
+
+	printf("ch변수의 값은 %d이고 ch의 메모리 할당 크기는 %dbytes 입니다.\n", ch, sizeof(ch));
+	printf("r1변수의 값은 %f이고 r1의 메모리 할당 크기는 %dbytes 입니다.\n", r1, sizeof(float));
+
+
+	// 배열(Array)
+	// 인덱싱은 배열에서 값을 가져오는 방법
+	// 인덱스는 배열의 원소들의 개수 - 1만큼 지정이 가능하다.
+	int nums[5] = {10,20,30,40,50};
+	printf("nums 배열의 첫번째 값 : %d\n", nums[0]);
+	printf("nums 배열의 두번째 값 : %d\n", nums[1]);
+	printf("nums 배열의 다섯번째 값 : %d\n", nums[4]);
+
+	nums[0] = 60;
+	//nums[0] =3.14F;
+	for (int i = 0; i < 5; i++)
+	{
+		printf("nums[%d] = %d\n", i, nums[i]);
+	}
+
+	//
+	short score[] = { 100,90,80,70 };
+	short sum = 0;
+	// score 배열의 모든 원소들의 합계를 구하는 코드를 작성하세요
+	for (short i = 0; i < 4; i++) //i가 0부터 4보다 작은동안 i값을 1씩 증가시키면서 반복
+	{
+		sum += score[i];
+	}
+	printf("총점 %d점\n:", sum);
+	printf("평균 %.2f점\n:", sum/4.F);
+
+	//int nums[5] = {10,20,30,40,50};
+	/*
+	nums[0] : 1
+	*/
+
+	for (int i = 0; i < 5; i++)
+	{
+		nums[i] = i+1;
+		printf("nums[%d] = %d\n", i, nums[i]);
+	}
+
+	printf("nums배열의 크기 : %d bytes\n", sizeof(nums));
+
+	int nums2[] = { 1,3,4,6,3,5,7,2,3,5,7,8,6,7,9,5,3,7,8,4,7,9,5,4,3,6,8,9,5,4,1 };
+	printf("nums2배열의 크기 : %d bytes\n", sizeof(nums2));
+	printf("nums2배열의 길이 : %d 개\n", sizeof(nums2)/sizeof(int));
+	int count = sizeof(nums2) / sizeof(int); // 배열의 원소들의 개수를 구함
+	for (int i = 0;i < count;i++)
+	{
+		printf("%d", nums2[i]);
+	}
+
+	return 0;
+}
+
---
+++ Lesson13/Lesson13.cpp
@@ -0,0 +1,59 @@
+﻿#include <stdio.h>
+#include<string.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+    /*char sz[] = { 65,66,67,68,69,70,71 };*/
+    //'\0' == 0 :null terminated char acter(널 터미네이티드 문자)
+    char sz[] = { 'A','B','C','D','E','F','G', '\0' };
+    // sz배열의 크기 : 7 byres
+    // sz배열의 길이 : 7 개
+
+    printf("sz배열의 크기 : %d bytes\n", sizeof(sz));
+    printf("sz배열의 길이 : %d 개\n", sizeof(sz) / sizeof(char));
+
+    int length = sizeof(sz) / sizeof(char); // 배열의 원소들의 개수를 구함
+    for (int i = 0;i < length;i++)
+    {
+        printf("%c", sz[i]);
+    }
+    printf("\n");
+
+    // for 문을 이용하여 sz배열에 있는 값들을 모두 소문자로 변경하세요
+
+    for (int i = 0; i < length - 1; i++)
+    {
+        sz[i] +=32;
+    }
+
+    printf("sz : %s\n", sz);
+    printf("sz 문자열의 길이 : %d\n", strlen(sz));
+
+    /*char sz2[] = { 'H','e','l','l','o',' ','W','o','r','l','d',0 };*/
+    char sz2[] = "Hello World";
+    printf("sz2 문자열의 길이 : %d\n", strlen(sz2));
+    printf("sz2 배열의 길이 : %d\n", sizeof(sz2));
+    for (int i = 0; i < strlen(sz2);i++)
+    {
+        printf("%c\n", sz2[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < strlen(sz2); i++)
+    {
+        if(sz2[i] >= 'a' && sz2[i] <= 'z') //sz[i]요소가 소문자이면
+            sz2[i] -= 32;
+    }
+
+    printf("%s\n", sz2);
+
+    char sz3[] = "안녕";
+    printf("%s\n", sz3);
+    printf("strlen(sz3) : %d\n", strlen(sz3));
+    printf("sizeof(sz3) : %d\n", sizeof(sz3));
+
+    return 0;
+}
+
